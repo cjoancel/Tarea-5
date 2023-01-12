ab2300bb6a08090598d27c42f264ff61059119f7
Joancel Castillo <powermetal_5000@hotmail.com>
2023-01-12 17:30:42 -0400

    Agregar archivos de proyecto.

---
+++ Exercise1if/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingresa tu nombre: ");
+string Nombre = Console.ReadLine();
+
+Console.WriteLine("Ingresa tu email: ");
+string email = Console.ReadLine();
+
+Console.WriteLine("Posee cupon? (s/n): ");
+string posee = Console.ReadLine();
+
+int descuento = 0;
+int precio = 25000;
+
+
+if (posee =="S" || posee == "s")
+{
+    Console.WriteLine("Ingresa el codigo del cupon: ");
+    string cupon = Console.ReadLine();
+
+    Console.WriteLine("Ingresa el % de descuento del cupon: ");
+    int porc = int.Parse(Console.ReadLine());
+
+    Console.WriteLine();
+    Console.WriteLine("Precio del producto: " + precio);
+    descuento = precio * porc / 100;
+  
+
+    Console.WriteLine("Precio con descuento: " + (precio-descuento));
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("Precio del producto: " + precio);
+}
+
+
---
+++ exercise2switch/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] lenguajes = new string[] { "C", "C#" , "C++" , "Java" , "Pyton" , "Rubi" };
+
+int i=1;
+foreach(string s in lenguajes)
+{
+    Console.WriteLine(i + ": " +s);
+    i++;
+}
+
+Console.WriteLine("Ingrese el numero del lenguaje deseado: ");
+int opc = int.Parse(Console.ReadLine());
+
+switch (opc)
+{
+    case 1:
+        Console.WriteLine("Ud eligio: "+ lenguajes[opc-1]);
+        break;
+    case 2:
+        Console.WriteLine("Ud eligio: " + lenguajes[opc - 1]);
+        Console.WriteLine("¡Hola Mundo!");
+        break;
+    case 3:
+        Console.WriteLine("Ud eligio: " + lenguajes[opc - 1]);
+        break;
+    case 4:
+        Console.WriteLine("Ud eligio: " + lenguajes[opc - 1]);
+        break;
+    case 5:
+        Console.WriteLine("Ud eligio: " + lenguajes[opc - 1]);
+        break;
+    case 6:
+        Console.WriteLine("Ud eligio: " + lenguajes[opc - 1]);
+        break;
+    default:
+        Console.WriteLine("Ud eligio: " + lenguajes[opc - 1]);
+        break;
+}
